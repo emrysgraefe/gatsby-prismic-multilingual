47fd19710280cc55232c2bcae700a120ab552552
Emrys Graefe <emrysgraefe@gmail.com>
2021-06-02 10:20:05 -0400

    Put the configuration object into an array

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -2,15 +2,15 @@ import * as React from 'react'
 import { withPrismicPreviewResolver } from 'gatsby-plugin-prismic-previews'
 import linkResolver from '../utils/linkResolver'
 
-const repositoryName = process.env.GATSBY_PRISMIC_CONTAINER_NAME
-
 const PreviewPage = () => {
   return (
     <p>Loading...</p>
   )
 }
 
-export default withPrismicPreviewResolver(PreviewPage, {
-  repositoryName,
-  linkResolver
-})
+export default withPrismicPreviewResolver(PreviewPage, [
+  {
+    repositoryName: process.env.GATSBY_PRISMIC_CONTAINER_NAME,
+    linkResolver
+  }
+])
