94920d9a8c0bb6d99ad68649e176568755aeccc7
Emrys Graefe <emrysgraefe@gmail.com>
2021-06-02 09:32:03 -0400

    Finish conversion to beta preview plugin

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -2,18 +2,13 @@ import * as React from 'react'
 import { withPrismicPreviewResolver } from 'gatsby-plugin-prismic-previews'
 import linkResolver from '../utils/linkResolver'
 
-const PreviewPage = ({ isPreview }) => {
-  if (isPreview === false) return 'Not a preview!'
+const PreviewPage = () => {
   return (
     <p>Loading...</p>
   )
 }
 
-const WrappedPreview = (props) => {
-  return withPrismicPreviewResolver(PreviewPage, {
-    repositoryName: process.env.GATSBY_PRISMIC_CONTAINER_NAME,
-    linkResolver
-  })(props)
-}
-
-export default WrappedPreview
+export default withPrismicPreviewResolver(PreviewPage, {
+  repositoryName: process.env.GATSBY_PRISMIC_CONTAINER_NAME,
+  linkResolver
+})
